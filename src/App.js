ce39906292d5a54b76d7a7cd236ab5a437fef31f
Arpan Srivastava <azs0239@auburn.edu>
2023-11-14 23:52:00 -0600

    comment comp. content posted to DB

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Layout from './components/layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
   try {
@@ -25,6 +29,17 @@ function App() {
 
 }
 
+const getMovieData = async (movieId) => {
+  try {
+    const response = await api.get(`/api/v1/movies/${movieId}`);
+    const singleMovie = response.data;
+    setMovie(singleMovie);
+    setReviews(Array.isArray(singleMovie.reviews) ? singleMovie.reviews : []);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
   useEffect(() => {
     getMovies();
   }, [])
@@ -35,7 +50,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
-
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path='/Reviews/:movieId' element = {<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews = {setReviews}/>}></Route>
         </Route>
       </Routes>
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
             <Navbar.Brand href="/" style={{"color":'gold'}}>
-                <FontAwesomeIcon icon={faVideoSlash} /> Gold
+                <FontAwesomeIcon icon={faVideoSlash} /> Movie Review Service
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll"/>
             <Navbar.Collapse>
